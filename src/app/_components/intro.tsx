4e7d9a0599464b27c54ec4cc6d5b90b39dac2224
Farimah Anvari <farimah.anvari@mia-care.io>
2024-12-18 09:47:02 +0100

    added home page call

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,10 +1,14 @@
+import { fetchCrudCollection } from "@/lib/api";
 import { CMS_NAME } from "@/lib/constants";
 
-export function Intro() {
+export async function Intro() {
+  const homePage = await fetchCrudCollection({endpoint: 'home-page'})
+
+  console.log('HOME PAGE', homePage)
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        {homePage[0].title}
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A statically generated blog example using{" "}
