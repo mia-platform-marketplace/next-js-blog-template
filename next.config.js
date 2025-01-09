a0eb53ac716d72f134370ab58c1aee5d2ab6a36f
Farimah Anvari <farimah.anvari@mia-care.io>
2025-01-09 11:54:31 +0100

    rollback the nextjs config

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
     output: 'standalone',
     reactStrictMode: true,
     serverRuntimeConfig: {
-      CRUD_PATH: process.env.CRUD_PATH ? process.env.CRUD_PATH : "REPLACE_SERVER_ENV_CRUD_PATH",
-      FILES_SERVICE_PATH: process.env.FILES_SERVICE_PATH ? process.env.FILES_SERVICE_PATH : "REPLACE_SERVER_ENV_FILES_SERVICE_PATH",
-      WEBSITE_BASE_PATH: process.env.WEBSITE_BASE_PATH ? process.env.WEBSITE_BASE_PATH : "REPLACE_SERVER_ENV_WEBSITE_BASE_PATH"
+      CRUD_PATH: "http://crud-service",
+      FILES_SERVICE_PATH: "REPLACE_SERVER_ENV_FILES_SERVICE_PATH",
+      WEBSITE_BASE_PATH: "https://jamstack-test.mia-demo-re5gu6.gcp.mia-platform.eu"
     },
     publicRuntimeConfig: {
       PUBLIC_FILES_PATH: '/files-service',
